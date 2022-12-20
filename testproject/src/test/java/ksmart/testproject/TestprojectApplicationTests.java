daa3c54d788d73d1d596c2c9162ddbeaa44a2611
ECS <ECS@DESKTOP-048HU3O>
2022-12-20 16:10:02 +0900

    [teamproject] 팀프로젝트 초기세팅

---
+++ testproject/src/test/java/ksmart/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
